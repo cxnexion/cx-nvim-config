7535760106e218b6c0dd0d181ca5c1151eceffc0
cxnexion <112370711+cxnexion@users.noreply.github.com>
2026-04-05 20:50:34 +0300

    changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,21 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    local config = require("nvim-treesitter.config")
-    config.setup({
-      auto_install = true,
-      ensure_installed = { "lua", "go", "rust", "javascript" },
-      highlight = { enable = true },
-      indent = { enable = true }
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	lazy = false,
+	config = function()
+		local config = require("nvim-treesitter.config")
+
+		config.setup({
+			auto_install = true,
+			ensure_installed = { "lua", "go", "rust", "javascript", "svelte" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start, buf)
+			end,
+		})
+	end,
 }

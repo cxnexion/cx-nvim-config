d55d72219f558e0da75d321c2f72ed1a651602bb
cxnexion <112370711+cxnexion@users.noreply.github.com>
2026-04-15 18:07:34 +0300

    Removed emoji bind in insert mode

--- lua/plugins/emoji.lua
+++ lua/plugins/emoji.lua
@@ -8,7 +8,6 @@ return {
 	config = function()
 		require("icon-picker").setup({ disable_legacy_commands = true })
 
-		vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", { desc = "Insert Icon" })
 		vim.keymap.set("n", "<leader>fi", "<cmd>IconPickerNormal<cr>", { desc = "Find Icon" })
 	end,
 }
